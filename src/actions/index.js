d5652da742ed44dda77a9a3b091a127616d671e6
Zac McClung <zac.mcclung370@gmail.com>
2018-05-19 23:26:51 -0600

    navigating between different routes

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,6 @@
 import firebase from 'firebase';
 import { EMAIL_CHANGED, PASSWORD_CHANGED, LOGIN_USER_SUCCESS, LOGIN_USER_FAIL, LOGIN_USER } from './types';
+import { Actions } from 'react-native-router-flux';
 
 export const emailChanged = text => {
   return {
@@ -44,4 +45,6 @@ const loginUserSuccess = (dispatch, user) => {
     type: LOGIN_USER_SUCCESS,
     payload: user
   });
+
+  Actions.employeeList();
 };
